3066eb82874c004ef15e2776b2e678f7bfc0aeaf
Alec Bridge <Alecbridge306@gmail.com>
2016-07-06 21:42:59 -0500

    added js/html skeleton to api

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function fetchJSON(url) {
+  return fetch(url).then(function(response) {
+    var contentType = response.headers.get("content-type");
+    if(contentType && contentType.indexOf("application/json") !== -1) {
+      return response.json();
+    } else {
+      console.log("Oops, we haven't got JSON!");
+    }
+  });
+}
+
+function marvelFactory(config) {
+  return function(path) {
+    var timestamp = new Date().getTime();
+    var hash = CryptoJS.MD5(timestamp + config.privateKey + config.publicKey).toString();
+    var url = config.hostname + '/v' + config.version + '/public' + path + '?apikey=' + config.publicKey + '&ts=' + timestamp + '&hash=' + hash;
+    console.log(url);
+
+    return fetchJSON(url);
+  }
+}
+
+// Get an instance of the marvel api
+var marvel = marvelFactory({
+  hostname: 'http://gateway.marvel.com',
+  publicKey: '9228da88dd1a2a154f40ddbaf5f7d37b',
+  privateKey: '1a274b4e099b1110ac94cb3772e82705be286c3f',
+  version: '1'
+});
+
+console.log('Hello Friends, Test Text')
